1c58676cc18eb9449c86176bc1eef47e8a8eaa60
alexanderpono <brain16383@yandex.ru>
2025-06-20 23:42:31 +0300

    [feconf-draft] Remove project template

--- lex-feconf-draft/app-dev/custom.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.png' {
-    const content: any;
-    export default content;
-}
-
-declare module '*.scss' {
-    const classes: { [key: string]: string };
-    export default classes;
-}
--- lex-feconf-draft/app-dev/src/store/store.ts
+++
@@ -1,9 +0,0 @@
-import { combineReducers, createStore, applyMiddleware, Store, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { appReducer } from './appReducer';
-
-const rootReducer = combineReducers({ app: appReducer });
-
-export const store = createStore(rootReducer, composeWithDevTools());
-
-export type RootState = ReturnType<typeof store.getState>;
